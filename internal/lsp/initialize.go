baff863223236bb72fb23ae5a98caeaa5e0f4a45
neet-007 <moayedeffect@gmail.com>
2024-11-18 20:10:35 +0300

    the lsp is initilized and connected to neovim

---
+++ internal/lsp/initialize.go
@@ -0,0 +1,54 @@
+package lsp
+
+type InitializeRequest struct {
+	Request
+	Params InitializeRequestParams `json:"params"`
+}
+
+type InitializeRequestParams struct {
+	ClientInfo *ClientInfo `json:"clientInfo"`
+}
+
+type ClientInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type InitializeResponse struct {
+	Response
+	Result InitializeResult `json:"result"`
+}
+
+type InitializeResult struct {
+	ServerCapabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo         ServerInfo         `json:"serverInfo"`
+}
+
+type ServerCapabilities struct {
+	TextDocumentSync   int            `json:"textDocumentSync"`
+	HoverProvider      bool           `json:"hoverProvider"`
+	DefinitionProvider bool           `json:"definitionProvider"`
+	CodeActionProvider bool           `json:"codeActionProvider"`
+	CompletionProvider map[string]any `json:"completionProvider"`
+}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func NewInitializeResponse(id int) InitializeResponse {
+	return InitializeResponse{
+		Response: Response{
+			RPC: "2.0",
+			Id:  &id,
+		},
+		Result: InitializeResult{
+			ServerCapabilities: ServerCapabilities{},
+			ServerInfo: ServerInfo{
+				Name:    "lox_lsp",
+				Version: "0.0.0",
+			},
+		},
+	}
+}
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+
+	"github.com/neet-007/lox_lsp_first/internal/lsp"
+	"github.com/neet-007/lox_lsp_first/pkg/rpc"
+)
+
+func main() {
+	logger := getLogger("/home/moayed/personal/lox_lsp_first/logs.txt")
+	logger.Println("Starting...")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(rpc.Split)
+
+	writer := os.Stdout
+
+	for scanner.Scan() {
+		msg := scanner.Bytes()
+		method, content, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("Error:%v", err)
+		}
+
+		handleMessage(logger, writer, "", method, content)
+
+	}
+}
+
+func handleMessage(logger *log.Logger, writer io.Writer, state string, method string, content []byte) {
+	logger.Printf("Message with method:%s\n", method)
+	switch method {
+	case "initialize":
+		{
+			var request lsp.InitializeRequest
+
+			if err := json.Unmarshal(content, &request); err != nil {
+				logger.Printf("Error: %v", err)
+				return
+			}
+
+			logger.Printf("Connected to: %s %s",
+				request.Params.ClientInfo.Version, request.Params.ClientInfo.Name)
+
+			response := lsp.NewInitializeResponse(request.Id)
+			writeResponse(writer, response)
+
+			logger.Println("reply sent")
+		}
+	}
+}
+
+func writeResponse(writer io.Writer, msg any) error {
+	reply := rpc.EncodeMessage(msg)
+
+	if _, err := writer.Write([]byte(reply)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getLogger(filePath string) *log.Logger {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	return log.New(file, "[LOX_LSP] ", log.Ldate|log.Lshortfile)
+}
